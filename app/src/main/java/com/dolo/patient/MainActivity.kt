5fd2bb82d4377c7c2db1d4bef9b10eb3017b26a6
bobysirji-creator <boby.sirji@gmail.com>
2026-07-12 19:36:29 +0530

    Update MainActivity.kt

--- app/src/main/java/com/dolo/patient/MainActivity.kt
+++ app/src/main/java/com/dolo/patient/MainActivity.kt
@@ -3,13 +3,14 @@ package com.dolo.patient
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.dolo.patient.auth.FakeAuthRepository
 import com.dolo.patient.ui.DoloPatientApp
 import com.dolo.patient.ui.theme.DoloTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { DoloTheme { DoloPatientApp() } }
+        val authRepository = FakeAuthRepository(getSharedPreferences("dolo_session", MODE_PRIVATE))
+        setContent { DoloTheme { DoloPatientApp(authRepository) } }
     }
 }
-
